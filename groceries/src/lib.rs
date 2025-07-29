8fca8ab4c4c2245b62db4e329f7e47855a4157bb
NjugunaWilfred-01 <wm453856@gmail.com>
2025-07-29 11:08:00 +0300

    feat: Create two functions: for adding an element in a vector and print the element in an index with the specified index

---
+++ groceries/src/lib.rs
@@ -0,0 +1,22 @@
+pub fn insert(vec: &mut Vec<String>, val: String) {
+    vec.push(val);
+}
+
+pub fn at_index(slice: &[String], index: usize) -> &str {
+    if index < slice.len() {
+        &slice[index]
+    } else {
+        panic!();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
