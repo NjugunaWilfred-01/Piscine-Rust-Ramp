0714e96deaac6451210b27e5c00b402d19977461
NjugunaWilfred-01 <wm453856@gmail.com>
2025-07-29 11:24:31 +0300

    feat: Add function that reverses the characters in a string

---
+++ reverse_string/src/lib.rs
@@ -0,0 +1,4 @@
+pub fn rev_str(input: &str) -> String {
+    input.chars().rev().collect() //for reversing the characters in a string
+    // input.chars().collect() // for reversing a string
+}
---
+++ reverse_string/target/debug/deps/reverse_string-7d5e327f32ae6602.d
@@ -0,0 +1,5 @@
+/home/wnjuguna/Piscine-Rust-Ramp/reverse_string/target/debug/deps/reverse_string-7d5e327f32ae6602.d: src/lib.rs
+
+/home/wnjuguna/Piscine-Rust-Ramp/reverse_string/target/debug/deps/libreverse_string-7d5e327f32ae6602.rmeta: src/lib.rs
+
+src/lib.rs:
