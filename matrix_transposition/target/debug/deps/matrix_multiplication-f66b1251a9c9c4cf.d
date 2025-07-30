1516d10c305f14de3c5a5410b5405b66c505a817
NjugunaWilfred-01 <wm453856@gmail.com>
2025-07-30 11:40:11 +0300

    Del: Remove unneeded files

--- matrix_transposition/target/debug/deps/matrix_multiplication-f66b1251a9c9c4cf.d
+++
@@ -1,5 +0,0 @@
-/home/wnjuguna/Piscine-Rust-Ramp/matrix_transposition/target/debug/deps/matrix_multiplication-f66b1251a9c9c4cf.d: src/lib.rs
-
-/home/wnjuguna/Piscine-Rust-Ramp/matrix_transposition/target/debug/deps/libmatrix_multiplication-f66b1251a9c9c4cf.rmeta: src/lib.rs
-
-src/lib.rs:
