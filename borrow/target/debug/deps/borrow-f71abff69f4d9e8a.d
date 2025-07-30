0e5054f95513d25f927a5079d297f0438f7471d1
NjugunaWilfred-01 <wm453856@gmail.com>
2025-07-30 11:11:59 +0300

    feat: create a function to get string length

---
+++ borrow/target/debug/deps/borrow-f71abff69f4d9e8a.d
@@ -0,0 +1,5 @@
+/home/wnjuguna/Piscine-Rust-Ramp/borrow/target/debug/deps/borrow-f71abff69f4d9e8a.d: src/lib.rs
+
+/home/wnjuguna/Piscine-Rust-Ramp/borrow/target/debug/deps/libborrow-f71abff69f4d9e8a.rmeta: src/lib.rs
+
+src/lib.rs:
